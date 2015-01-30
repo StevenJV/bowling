bd69182c9b2028a157f2f3f57d7bc3a336d489f7
StevenJV <steven@vorefamily.net>
2015-01-30 10:37:28 -0500

    start kata from here

---
+++ Bowling/Bowling.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bowling
+{
+  public class BowlingGame
+  {
+    private int _score = 0;
+    public void Roll(int pins) {
+      _score += pins;
+    }
+
+    public int Score() {
+      return _score;
+    }
+
+  }
+}
---
+++ BowlingTests/WhenASingleBallIsRolled.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bowling;
+using Xunit;
+using Xunit.Should;
+
+namespace BowlingTests
+{
+  public class BowlingGameTests
+  {
+    private BowlingGame game = new BowlingGame();
+
+    [Fact]
+    public void SingleDigitPinsAreScoredCorrectly()
+    {
+      game.Roll(1);
+      game.Score().ShouldBe(1);
+
+    }
+  }
+}
