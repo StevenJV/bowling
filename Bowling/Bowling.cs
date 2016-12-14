ed1c995cddd7976e8c3c1119c5de7c887184340c
StevenJV <StevenVore@tnwinc.com>
2016-12-14 12:02:54 -0500

    first Frame tests passing

--- Bowling/Bowling.cs
+++ Bowling/Bowling.cs
@@ -2,16 +2,22 @@
 
 namespace Bowling
 {
-  public class BowlingGame
-  {
-    private int _score = 0;
-    public void Roll(int pins) {
-      _score += pins;
-    }
+    public class BowlingGame
+    {
+        private int _score = 0;
+        public void Roll(int pins)
+        {
+            _score += pins;
+            if (_score > 10)
+            {
+                _score = 10;
+            }
+        }
 
-    public int Score() {
-      return _score;
-    }
+        public int Score()
+        {
+            return _score;
+        }
 
-  }
+    }
 }
---
+++ BowlingTests/FirstFrame.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Bowling;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Assert = Xunit.Assert;
+
+namespace BowlingTests
+{
+    public class FirstFrame
+    {
+        private readonly BowlingGame _game = new BowlingGame();
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(9)]
+        public void FirstBallSingleDigitShouldScoreSingleDigit(int firstBallPins)
+        {
+            _game.Roll(firstBallPins);
+            Assert.Equal(_game.Score(), firstBallPins);
+        }
+
+        [Fact]
+        public void FirstBallTenShouldScoreTen()
+        {
+            _game.Roll(10);
+            Assert.Equal(_game.Score(), 10);
+        }
+
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(5, 5)]
+        public void SecondBallAddsToFirstBallScore(int firstBallPins, int secondBallPins)
+        {
+            _game.Roll(firstBallPins);
+            _game.Roll(secondBallPins);
+            Assert.Equal(_game.Score(), firstBallPins + secondBallPins);
+        }
+
+        [Theory]
+        [InlineData(1, 9)]
+        [InlineData(5, 6)]
+        [InlineData(8, 6)]
+        public void FrameScoreCannotExceedTen(int firstBallPins, int secondBallPins)
+        {
+            _game.Roll(firstBallPins);
+            _game.Roll(secondBallPins);
+            Assert.InRange(_game.Score(),0,10);
+        }
+    }
+}
