de0ddd95e25876b1e6ed628f211f90cce7ec09f6
StevenJV <steven@vorefamily.net>
2019-08-23 14:32:13 -0400

    Score the first thrown ball correctly

---
+++ tests/FirstFrame.cs
@@ -0,0 +1,24 @@
+﻿using Bowling;
+using Xunit;
+using Xunit.Sdk;
+
+namespace Tests
+{
+    public class FirstFrame
+    {
+        private readonly BowlingGame _game = new BowlingGame();
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(5)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void FirstBallIsScoredAsThrown(int firstBallPins)
+        {
+            _game.Roll(firstBallPins);
+            Assert.Equal(firstBallPins, _game.Score());
+        }
+
+    }
+}
