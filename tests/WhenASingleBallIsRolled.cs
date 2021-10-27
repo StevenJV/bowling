a3ac44716872d1e8f002f347453c4716344a4ca0
StevenJV <steven@vorefamily.net>
2021-10-27 14:19:15 -0400

    valid single-roll first frame

--- tests/WhenASingleBallIsRolled.cs
+++ tests/WhenASingleBallIsRolled.cs
@@ -7,11 +7,22 @@ namespace BowlingTests
   {
     private BowlingGame game = new BowlingGame();
 
-    [Fact]
-    public void SingleDigitPinsAreScoredCorrectly()
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(1, 1)]
+    [InlineData(2, 2)]
+    [InlineData(3, 3)]
+    [InlineData(4, 4)]
+    [InlineData(5, 5)]
+    [InlineData(6, 6)]
+    [InlineData(7, 7)]
+    [InlineData(8, 8)]
+    [InlineData(9, 9)]
+    [InlineData(10, 10)]
+    public void SingleDigitPinsAreScoredCorrectly(int value1, int expected)
     {
-      game.Roll(1);
-      Assert.Equal(1, game.Score());
+      game.Roll(value1);
+      Assert.Equal(expected, game.Score());
 
     }
   }
